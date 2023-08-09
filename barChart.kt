ec1e4ea315ca7d23667fa87588749c896889c4b2
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-08-09 14:31:54 -0500

    Create barChart.kt

---
+++ barChart.kt
@@ -0,0 +1,7 @@
+fun main() {
+val stores = listOf("Enter today's sales for store 1: 1000 [Enter]", "Enter today's sales for store 2: 1200 [Enter]", "Enter today's sales for store 3: 1800 [Enter]", "Enter today's sales for store 4: 800 [Enter]","Enter today's sales for store5: 1900 [Enter]"
+)
+for (store in stores) {
+    println(store)
+}
+}
