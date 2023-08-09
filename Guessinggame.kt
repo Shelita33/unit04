89bb18cf5081eaea0b2a569318ec15f195116474
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-08-08 22:03:57 -0500

    Guessinggame.kt

--- Guessinggame.kt
+++ Guessinggame.kt
@@ -1,1 +1,15 @@
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
+fun main() {
+    val secretNumber = (Math.random()*100).roundToInt()
+
+    println("Can you the guess the number I'm thinking of? (1-100)")
+   val guess = Integer. parseInt(readLine())
+ 
+ if (guess == secretNumber) {
+     println("You did it!! Wow Congrats:)")
+} else { 
+ println("Better luck next time!")
+ }
+}
